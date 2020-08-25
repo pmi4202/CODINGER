ccdd27a5376a355abf1307c9e64d4bde64657f13
pmi4202 <pmi4202@naver.com>
2020-08-26 01:32:40 +0900

    store_login back

--- DeliveryApp/views.py
+++ DeliveryApp/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 
 def store_login(request):
+    if request.method == 'POST': # POST 방식으로 request를 통해 정보가 오면 
+        # 변수(좌변이 변수이름)에 저장시켜 사용한다.
+        username = request.POST['id']
+        password = request.POST['password']
+        # 로그인
+        user = auth.authenticate(request, username=username, password=password)
+        # 성공
+        if user is not None:
+            auth.login(request, user)
+            return redirect('store_home')
+        # 실패
+        else:
+            return render(request, 'store_login.html', {'error': 'id or password is incorrect.'})
+    else:
+        return render(request, 'store_login.html')
+        
+def store_logout(request):
+    if request.method == 'POST': # POST 방식으로 request를 통해 정보가 오면
+        # 로그아웃
+        auth.logout(request)
+        return redirect('store_home')
     return render(request, 'store_login.html')
+
 
 #로그인 후, 처음 보이는 화면(디자인은 없음..)
 def store_home(request):
