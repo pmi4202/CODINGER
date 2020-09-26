c62c2f19d7bcf2e7eabb93f5839a6f406edbf357
pmi4202 <pmi4202@naver.com>
2020-09-26 13:25:19 +0900

    user_login back

--- DeliveryUserApp/views.py
+++ DeliveryUserApp/views.py
@@ -4,9 +4,28 @@ from DeliveryApp.models import Store, Menu, Order, DeliveryPrice, User, Delivery
 # Create your views here.
 #앱 로그인
 def user_login(request):
-  return render(request, 'user_login.html')
+  if request.method == 'POST': # POST 방식으로 request를 통해 정보가 오면 
+      # 변수(좌변이 변수이름)에 저장시켜 사용한다.
+      username = request.POST['id']
+      password = request.POST['password']
+      # 로그인
+      users = User.objects.filter(userName= username)
+      if users:
+        user = auth.authenticate(request, username=username, password=password)
+
+      # 성공
+      if user is not None:
+          auth.login(request, user)
+          return redirect('store_order')
+      # 실패
+      else:
+          return render(request, 'user_login.html', {'error': 'id or password is incorrect.'})
+  else:
+      return render(request, 'user_login.html')
+
 #앱 회원가입
 def user_signup(request):
+  
   return render(request, 'user_signup.html')
 #앱 메인
 def user_home(request):
